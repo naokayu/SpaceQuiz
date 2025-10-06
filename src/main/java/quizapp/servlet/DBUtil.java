e72d75e8c27c07f8500dc0b37238eb351b2f4be4
Naoki Yamanobe <bfywt32814@yahoo.co.jp>
2025-10-06 15:18:10 +0900

    mysqlに変更

--- src/main/java/quizapp/servlet/DBUtil.java
+++ src/main/java/quizapp/servlet/DBUtil.java
@@ -5,22 +5,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    // パスは / に統一したほうが安全
-    private static final String URL = "jdbc:sqlite:C:/sqlite/quiz.db";
+    private static final String URL = "jdbc:mysql://localhost:3306/quizdb?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root"; // MySQLのユーザー名
+    private static final String PASSWORD = "pass"; // あなたのMySQLパスワード
 
     public static Connection getConnection() throws SQLException {
         try {
-            System.out.println("Loading SQLite driver...");
-            Class.forName("org.sqlite.JDBC");
+            System.out.println("Loading MySQL driver...");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver loaded OK!");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-            throw new SQLException("SQLite JDBC Driver not found.", e);
+            throw new SQLException("MySQL JDBC Driver not found.", e);
         }
 
         System.out.println("Connecting to: " + URL);
-
-        Connection conn = DriverManager.getConnection(URL);
+        Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
         System.out.println("Connection OK: " + conn);
         return conn;
     }
